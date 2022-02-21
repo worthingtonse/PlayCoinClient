081a951def13af738cd372dd460deb533592b62e
Sean Worthington <sean@worthington.net>
2022-02-20 22:09:37 -0800

    Update RAIDA.cs

--- Drafts/RAIDA.cs
+++ Drafts/RAIDA.cs
@@ -84,7 +84,7 @@ namespace Founders4
 
             var taskList = new List<Task> { t00, t01, t02, t03, t04, t05, t06, t07, t08, t09, t10, t11, t12, t13, t14, t15, t16, t17, t18, t19, t20, t21, t22, t23, t24 };
          
-            Task.WaitAll(taskList.ToArray(), milliSecondsToTimeOut);
+            Task.WaitAll(taskList.ToArray(), EchoLog.twoStandardDeviatons);
          
             return results;
         }//end echo
